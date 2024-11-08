83b3b39bc8dd3917244291f71179e2229b4293ca
Angelinic05 <152038826+Angelinic05@users.noreply.github.com>
2024-11-08 10:50:14 -0500

    upd

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 function loadImage(url) {
     return new Promise(resolve => {
         const xhr = new XMLHttpRequest();
@@ -21,23 +19,40 @@ function loadImage(url) {
 let signaturePad = null;
 
 window.addEventListener('load', async () => {
-    const canvas = document.querySelector("canvas");
+    const canvas = document.querySelector("#signature-canvas");
     canvas.height = canvas.offsetHeight;
     canvas.width = canvas.offsetWidth;
 
     signaturePad = new SignaturePad(canvas, {});
 
     const form = document.querySelector('#form');
-    form.addEventListener('submit', (e) => {
+    form.addEventListener('submit', async (e) => {
         e.preventDefault();
 
+        let email = document.getElementById('email').value;
         let nombres = document.getElementById('nombre').value;
         let apellidos = document.getElementById('apellido').value;
         let identificacion = document.getElementById('identificacion').value;
 
-        generatePDF(nombres, apellidos, identificacion);
+        // Validar el correo electrónico
+        const isEmailValid = await validateEmail(email);
+        const messageElement = document.getElementById('message');
+
+        if (isEmailValid) {
+            messageElement.textContent = '';
+            generatePDF(nombres, apellidos, identificacion);
+        } else {
+            messageElement.textContent = 'No está registrado.';
+            messageElement.style.color = 'red';
+        }
     });
 });
+
+async function validateEmail(email) {
+    const response = await fetch(`https://script.google.com/macros/s/AKfycbxLmulSsfmXnD-5sAAd8D7cnDXl4UAz-eP3DsTA-P5Xv21nhfaX_Z_ti2NgNP6tCEm8/exec?email=${encodeURIComponent(email)}`);
+    const data = await response.json();
+    return data.found; // Retorna true si el correo está registrado
+}
 
 async function generatePDF(nombres, apellidos, identificacion) {
     const image = await loadImage("formulario.jpg"); // Asegúrate de que esta imagen esté en la ruta correcta
